267445ae37a406bb4515be92ebe8df98ecb9fa58
Kristjan Vingel <kristjan.vingel.protonmail.ch>
2022-11-03 15:17:46 +0700

    Buttons refactored

---
+++ Meditation Timer/Meditation Timer/CircleButton.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButton.swift
+//  Meditation Timer
+//
+//  Created by Kristjan Vingel on 03.11.2022.
+//
+
+import SwiftUI
+
+struct CircleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButton()
+    }
+}
---
+++ Meditation Timer/Meditation Timer/StartButton.swift
@@ -0,0 +1,20 @@
+//
+//  StartButton.swift
+//  Meditation Timer
+//
+//  Created by Kristjan Vingel on 03.11.2022.
+//
+
+import SwiftUI
+
+struct StartButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButton()
+    }
+}
